0f14ad53bdf9790db25272cb7608e2e13ef94346
Michelle <56207268+MichelleGray78@users.noreply.github.com>
2021-08-05 10:00:45 +0100

    new changes

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error')
+const connectDB = require('./config/db');
 
 //Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
+
+//Route files
+const foods = require('./routes/foods')
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+app.use(morgan('dev'));
+}
+
+//Mount routers
+app.use('/api/v1/foods', foods);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 8080;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta.bold)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server and exit process
+    server.close(() => process.exit(1));
+});
